151efaf2a403ed3a28371df550b26ddf10fe0de3
imanolof29 <imanolof29@gmail.com>
2023-02-14 12:03:44 +0100

    Initial Commit

---
+++ Tasky/TaskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskyApp.swift
+//  Tasky
+//
+//  Created by Imanol Ortiz on 14/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
